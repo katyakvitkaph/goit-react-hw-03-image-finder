3a18ded530aae161ea3b1a2124cd68e6a408faf8
katyakvitkaph <kvitka.katja@gmail.com>
2019-11-29 22:25:38 +0200

    Updates

---
+++ precache-manifest.c8dfdad450cc1d1c9e713c79183296ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9a44ebb811a079c4e9735902ccb937",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "0ce30f129494e24aa266",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "0123f541bf0138cd2119",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.ae1c1124.chunk.css"
+  },
+  {
+    "revision": "0ce30f129494e24aa266",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.89a8d4c4.chunk.js"
+  },
+  {
+    "revision": "0123f541bf0138cd2119",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.9a5d3e61.chunk.js"
+  },
+  {
+    "revision": "3dc2d4bc992ea5be91b7",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.7345331c.js"
+  }
+]);
